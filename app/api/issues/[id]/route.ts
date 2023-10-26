54d98d084eec7c8e9ae6e6b5d9524ebdbe15cc1d
Bilal Aslam <m.bialaslam82@gmail.com>
2023-10-26 21:06:39 +0500

    Build an API for deleting an issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -32,3 +32,20 @@ export async function PATCH(request : NextRequest, {params} : {params : {id : st
 
 
 }
+
+
+
+export async function DELETE(request : NextRequest, {params} : {params : {id : string}}) {
+    
+    const issue = await prisma.issue.findUnique({
+        where : { id: parseInt(params.id)}
+    });
+    if(!issue) return NextResponse.json({error: 'Invalid Issue.'});
+
+    prisma.issue.delete({
+        where : {id : issue.id}
+    })
+
+    return NextResponse.json({})
+    
+}
