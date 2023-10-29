2b0ab81c2268a3ab9e5350658ee36010a96ab8f1
Bilal Aslam <m.bialaslam82@gmail.com>
2023-10-29 14:29:26 +0500

    Fetch datat with react query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -2,20 +2,32 @@
 import prisma from "@/prisma/client";
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
 
 function AssigneeSelect() {
 
-    const [users, setUsers] = useState<User[]>([]);
+    const {data: users, error, isLoading} = useQuery<User[]>({
+        queryKey : ['users'],
+        queryFn: () => axios.get('/api/users').then(res =>res.data ),
+        staleTime: 1000, // 60s
+        retry: 5
+    });
+
+
+    if(isLoading)return <Skeleton/>
+    if(error) return null;
+    // const [users, setUsers] = useState<User[]>([]);
     
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const {data} = await axios.get<User[]>('/api/users');
-            setUsers(data);
-        }
-        fetchUsers();
-    },[])
+    // useEffect(() => {
+    //     const fetchUsers = async () => {
+    //         const {data} = await axios.get<User[]>('/api/users');
+    //         setUsers(data);
+    //     }
+    //     fetchUsers();
+    // },[])
     
   return (
     <Select.Root defaultValue="apple">
@@ -23,7 +35,7 @@ function AssigneeSelect() {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map(user => (
+          {users?.map(user => (
               <Select.Item key={user.id} value={user.id}>{user.email}</Select.Item>
 
           ))}
