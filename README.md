cdba1de0c6147aff850703f2398759dfeb6f73ed
Bilal Aslam <m.bialaslam82@gmail.com>
2023-10-29 23:38:26 +0500

    Show the toast message

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Thirdparty Helper Libararies
 4) npm install -D @tailwindcss/typography
 5) npm i @radix-ui/react-icons
 6) npm i  @next-auth/prisma-adapter@1.0.7
+7) npm i @radix-ui/themes@1.1.2
+8) npm i @tanstack/react-query@4.35.3
+9) npm install react-hot-toast
--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,49 +1,64 @@
-'use client'
+"use client";
 import prisma from "@/prisma/client";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import Skeleton from "react-loading-skeleton";
 
-function AssigneeSelect({issue} : {issue : Issue}) {
+function AssigneeSelect({ issue }: { issue: Issue }) {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 1000, // 60s
+    retry: 5,
+  });
 
-    const {data: users, error, isLoading} = useQuery<User[]>({
-        queryKey : ['users'],
-        queryFn: () => axios.get('/api/users').then(res =>res.data ),
-        staleTime: 1000, // 60s
-        retry: 5
-    });
+  if (isLoading) return <Skeleton />;
+  if (error) return null;
+  // const [users, setUsers] = useState<User[]>([]);
 
+  // useEffect(() => {
+  //     const fetchUsers = async () => {
+  //         const {data} = await axios.get<User[]>('/api/users');
+  //         setUsers(data);
+  //     }
+  //     fetchUsers();
+  // },[])
 
-    if(isLoading)return <Skeleton/>
-    if(error) return null;
-    // const [users, setUsers] = useState<User[]>([]);
-    
-    // useEffect(() => {
-    //     const fetchUsers = async () => {
-    //         const {data} = await axios.get<User[]>('/api/users');
-    //         setUsers(data);
-    //     }
-    //     fetchUsers();
-    // },[])
-    
   return (
-    <Select.Root  defaultValue={issue.assignedToUserId || ""} onValueChange={(userId) => {
-      axios.patch('/api/issues/'+ issue.id,{assignedToUserId : userId || null})
-    }}>
-      <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="">Unassigned</Select.Item>
-          {users?.map(user => (
-              <Select.Item key={user.id} value={user.id}>{user.email}</Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || ""}
+        onValueChange={(userId) => {
+          axios
+            .patch("/api/issues/" + issue.id, {
+              assignedToUserId: userId || null,
+            })
+            .then((data) => toast.success("Changes have been updated."))
+            .catch((error) => toast.error("Changes couldn't be saved."));
+        }}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.email}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster/>
+    </>
   );
 }
 
